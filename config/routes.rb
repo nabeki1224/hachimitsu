9a51becf99cd744b7e1b683cc2ccf65447675096
nabeki1224 <watanabe.kyo.infratop@gmail.com>
2021-08-27 13:12:58 +0900

    Add linelogin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    omniauth_callbacks: "omniauth_callbacks",
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }  
   root to: "home#top"
   resources :users, only: [:index, :show]
   resources :mitsus, only: [:index, :show, :new, :destroy, :create] do
